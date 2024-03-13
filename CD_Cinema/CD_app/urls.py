fd68a7e4e24b4db051e7fe5d969417408b4dd3ee
Arkadiy <av2828015@gmail.com>
2024-03-13 19:52:42 +0600

    secon init

--- CD_Cinema/CD_app/urls.py
+++ CD_Cinema/CD_app/urls.py
@@ -6,6 +6,10 @@ urlpatterns =[
     path('movies/top/', views.top_movies_page, name='top_movies_page'),
     path('movies/all/', views.all_movies_page, name='all_movies_page'),
     path('movies/detail/', views.movie_detail_page, name='movie_detail_page'),
-    path('movies/category/', views.movies_by_category_page, name='movies_by_category_page'),
+    path('movies/category/<slug:slug>/', views.movies_by_category_page, name='movies_by_category_page'),
     path('movies/search/', views.movies_by_search_page, name='movies_by_search_page'),
+
+    path('sign-up/', views.sign_up_page, name='sign_up_page'),
+    path('login/', views.login_page, name='login_page'),
+    path('logout/', views.logout_action, name='logout_action')
 ]
